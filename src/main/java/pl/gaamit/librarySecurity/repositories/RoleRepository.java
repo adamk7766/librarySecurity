93a77b71361baa856c3fc719387c2de793500606
Adam Krygier <adamk7766@gmail.com>
2018-04-05 14:43:57 +0200

    a

--- src/main/java/pl/gaamit/librarySecurity/repositories/RoleRepository.java
+++ src/main/java/pl/gaamit/librarySecurity/repositories/RoleRepository.java
@@ -1,12 +1,13 @@
 package pl.gaamit.librarySecurity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.gaamit.librarySecurity.model.Role;
 
 
 @Repository("roleRepository")
-public interface RoleRepository extends JpaRepository<Role, Integer>{
+public interface RoleRepository extends CrudRepository<Role, Integer> {
     Role findByRole(String role);
 
 }
--- src/main/java/pl/gaamit/librarySecurity/repositories/UserRepository.java
+++ src/main/java/pl/gaamit/librarySecurity/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package pl.gaamit.librarySecurity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.gaamit.librarySecurity.model.User;
 
 @Repository("userRepository")
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
 }
